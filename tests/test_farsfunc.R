ca4f2b6ee4d261963021f912b5cd53a60b7680cf
MoRudy <mikekimpdx@icloud.com>
2017-10-20 17:32:16 -0700

    Error Testing Correction--Error Message

--- tests/test_farsfunc.R
+++ tests/test_farsfunc.R
@@ -1,8 +1,8 @@
 library(testthat)
 library(farsfunc)
 
-expect_error(make_filename(yyyy),"Error in make_filename(yyyy) : object 'yyyy' not found")
+expect_error(make_filename(yyyy),"object 'yyyy' not found")
 
-expect_error(fars_read(coursera),"Error in file.exists(filename) : object 'coursera' not found")
+expect_error(fars_read(coursera),"object 'coursera' not found")
 
 expect_equal(make_filename(2013), "accident_2013.csv.bz2")
